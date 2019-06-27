dcf704eab9ee8d6915d6fa0abc9726ff4f9ef473
mlabarrere <mickael.labarrere@gmail.com>
2019-06-27 12:40:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="adansonia",
-    version="0.0.5",
+    version="0.0.6",
     author="mlabarrere",
     author_email="mlabarrere@baobab.bz",
     description="Utils package for common operations",
